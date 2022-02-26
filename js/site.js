3ee0d325fcc8cc122ea483063951cd141cccf3b9
marcm-pro <contact@marcm.pro>
2022-02-26 16:10:46 -0500

    Prepared the site for the application
    Created the necessary logos, modified the app.html page, and laid out the JavaScript file.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,18 @@
-// Call helloWorld
-function helloWorld() {
-    alert("Hello World");
+
+//get the values from the page
+//start or controller function
+function getValues() {
+
+}
+
+//generate numbers from the start value to the end value
+//logic function(s)
+function generateNumbers() {
+
+}
+
+//display the numbers and mark the even numbers bold
+//display or view function
+function displayNumbers() {
+
 }
